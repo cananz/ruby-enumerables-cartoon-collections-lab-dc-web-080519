6710ec4600397c43bf644b031008d563b5355f19
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-26 18:01:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,12 +11,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
   
-
+def long_planeteer_calls(calls)
   
-
-
-def long_planeteer_calls# code an argument here
-  # Your code here
 end
 
 def find_the_cheese# code an argument here
