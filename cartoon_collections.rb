c84a4bbacaafffe4ad60c8440930b26033659480
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-26 17:59:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,13 +9,12 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |element|
     element.capitalize + '!'
   end
-  
 return planeteer_calls
-
-  
-
-  
 end
+  
+
+  
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
